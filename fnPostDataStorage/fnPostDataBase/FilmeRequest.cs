862e15fba16c5a275c727902bbb468a1fa06a781
eufrauzino <douglas_e@outlook.com.br>
2025-01-31 01:14:00 -0300

    Criando uma Azure Functions para salvar em CosmosDB

---
+++ fnPostDataStorage/fnPostDataBase/FilmeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fnPostDataBase
+{
+    internal class FilmeRequest
+    {
+        public string id { get { return Guid.NewGuid().ToString(); } } // gera um novo GUID para o id
+        public string titulo { get; set; }
+        public string ano { get; set; }
+        public string video { get; set; }
+        public string thumb { get; set; }
+    }
+}
